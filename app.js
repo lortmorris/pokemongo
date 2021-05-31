0f47a8dc1a01be110b4e73cac72bb7cec7160583
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2021-05-30 21:26:17 -0300

    working

--- app.js
+++ app.js
@@ -3,66 +3,65 @@
  * Powered By César Casas
  */
 
-const http = require("http");
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const mongojs = require("mongojs");
-const server = http.createServer(app);
-const config = require("config");
-const debug = require("debug")("pokemongo");
-const db = mongojs(config.get("mongodb").host, config.get("mongodb").collections);
-const PORT = process.env.PORT || 5000
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongojs = require('mongojs');
+const config = require('config');
+const debug = require('debug')('pokemongo');
 
-app.use(express.static("./public"));
-app.use(bodyParser.urlencoded({extended: true}));
+const db = mongojs(config.get('mongodb').host, config.get('mongodb').collections);
+const PORT = process.env.PORT || 5000;
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.post('/getpoints', (req, res) => {
+  debug(`/getpoints called: ${JSON.stringify(req.body.query)}`);
 
-app.post("/getpoints", (req, res)=> {
+  const query = {
+    location: {
+      $near: {
+        $geometry: {
+          type: 'Point',
+          coordinates: [parseFloat(req.body.query.long), parseFloat(req.body.query.lat)],
+        },
+        $maxDistance: parseInt(req.body.query.distance, 10),
+      },
+    },
+  };
 
-    debug("/getpoints called: ", JSON.stringify(req.body.query));
+  if (req.body.query.type && req.body.query.type !== 'all') query.type = req.body.query.type;
 
-    let query = {
-        location: {
-            $near: {
-                $geometry: {
-                    type: "Point",
-                    coordinates: [parseFloat(req.body.query.long), parseFloat(req.body.query.lat)]
-                },
-                $maxDistance: parseInt(req.body.query.distance)
-            }
-        }
-    };
-
-    if(req.body.query.type && req.body.query.type!='all') query.type = req.body.query.type;
-
-    db.markers.find(query, {}, (err, docs)=> res.json(docs));
-
+  db.markers.find(query, {}, (err, docs) => res.json(docs));
 });
 
-app.post("/savemarkers", (req, res)=> {
+app.post('/savemarkers', (req, res) => {
+  debug(`/savemarkers called: ${JSON.stringify(req.body)}`);
+  const response = {
+    error: false,
+    msg: '',
+    result: null,
+  };
 
-    debug("/savemarkers called: ", JSON.stringify(req.body));
-    let response = {error: false, msg: "", result: null};
+  req.body.points.forEach((p) => {
+    p.location.coordinates[0] = parseFloat(p.location.coordinates[0]);
+    p.location.coordinates[1] = parseFloat(p.location.coordinates[1]);
+  });
 
-    req.body.points.forEach(p=> {
-        p.location.coordinates[0] = parseFloat(p.location.coordinates[0]);
-        p.location.coordinates[1] = parseFloat(p.location.coordinates[1]);
-    });
+  db.markers.insert(req.body.points, (err, docs) => {
+    if (err) {
+      response.error = true;
+      response.msg = err;
+    } else {
+      response.result = docs;
+    }
 
-    db.markers.insert(req.body.points, (err, docs)=> {
-        if (err) {
-            response.error = true;
-            response.msg = err;
-        } else {
-            response.result = docs;
-        }
-
-        res.json(response);
-    })
+    res.json(response);
+  });
 });
 
-app.listen(PORT, (err)=>{
-  err ? console.log('error') : console.log('listen on *:', PORT);
-});
+server.listen(PORT, () => console.log('listen on *:', PORT));
--- public/map.js
+++ public/map.js
@@ -1,119 +1,120 @@
-var map;
-var infowindow;
-var bsas = {lat: -34.595129, lng: -58.444244};
+const bsas = {
+  lat: -34.595129,
+  lng: -58.444244,
+};
+
+let map = null;
+let infowindow = null;
+
+
 var currentMarkers = [];
 var ashMarker = null;
 
-
 function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: bsas,
+    zoom: 13,
+  });
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: bsas,
-        zoom: 13
-    });
+  ashMarker = new google.maps.Marker({
+    map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: bsas,
+    icon: '/images/ash.png',
+  });
 
+  ashMarker.addListener('click', (event) => {
+    if (ashMarker.getAnimation() !== null) {
+      ashMarker.setAnimation(null);
+    } else {
+      ashMarker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  });
 
-    ashMarker = new google.maps.Marker({
-        map: map,
-        draggable: true,
-        animation: google.maps.Animation.DROP,
-        position: bsas,
-        icon: "/images/ash.png"
-    });
+  ashMarker.addListener('dragend', (event) => {
+    clearMarkers();
+    getPoints(event.latLng.lng(), event.latLng.lat(), 100, 'all');
+  });
 
-    ashMarker.addListener('click', function (event) {
-        if (ashMarker.getAnimation() !== null) {
-            ashMarker.setAnimation(null);
-        } else {
-            ashMarker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    });
-
-    ashMarker.addListener('dragend', function (event) {
-        clearMarkers();
-        getPoints(event.latLng.lng(), event.latLng.lat(), 100, 'all');
-    });
-
-
-    /*
-    setInterval(function () {
-        try {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                let coords = {lng: position.coords.longitude, lat: position.coords.latitude};
-                ashMarker.setPosition(new google.maps.LatLng(coords.lat, coords.lng));
-                getPoints(coords.lng,coords.lat,  100, 'all');
-            });
-        } catch (e) {
-            console.log("without position");
-        }
-
-    }, 1000);
-
-*/
-
+  setInterval(() => {
+    try {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const coords = {
+          lng: position.coords.longitude,
+          lat: position.coords.latitude,
+        };
+        ashMarker.setPosition(new google.maps.LatLng(coords.lat, coords.lng));
+        getPoints(coords.lng, coords.lat, 100, 'all');
+      });
+    } catch (e) {
+      console.log('without position');
+    }
+  }, 1000);
 }
-
 
 function getPoints(long, lat, distance, type) {
+  if (long === 0) long = bsas.lng;
+  if (lat === 0) lat = bsas.lat;
+  if (distance === 0) distance = 1000000000;
 
-    if (long == 0) long = bsas.lng;
-    if (lat == 0) lat = bsas.lat;
-    if (distance == 0) distance = 1000000000;
-
-    $.ajax({
-        type: "POST",
-        url: "/getpoints",
-        data: {query: {long: long, lat: lat, distance: distance, type: type}},
-        success: function (data) {
-
-            data.forEach(p => {
-                p.location.lng = p.location.coordinates[0];
-                p.location.lat = p.location.coordinates[1];
-                let icon = "/images/pokestop.png";
-                if (p.type == "pokemon") {
-                    p.photos[0] = "/images/pokemons/" + p.photos[0];
-                    icon = p.photos[0];
-                }
-                createMarker(p, icon);
-            });
-        },
-        dataType: "json"
-    });
+  $.ajax({
+    type: 'POST',
+    url: '/getpoints',
+    data: {
+      query: {
+        long,
+        lat,
+        distance,
+        type,
+      },
+    },
+    success: (data) => {
+      data.forEach((p) => {
+        p.location.lng = p.location.coordinates[0];
+        p.location.lat = p.location.coordinates[1];
+        let icon = '/images/pokestop.png';
+        if (p.type === 'pokemon') {
+          p.photos[0] = `/images/pokemons/${p.photos[0]}`;
+          icon = p.photos[0];
+        }
+        createMarker(p, icon);
+      });
+    },
+    dataType: 'json',
+  });
 }
 
-
 function clearMarkers() {
-    currentMarkers.forEach(m=> m.setMap(null));
-    currentMarkers = [];
+  currentMarkers.forEach((m) => m.setMap(null));
+  currentMarkers = [];
 }
 
 function createMarker(item, icon) {
-    var marker = new google.maps.Marker({
-        map: map,
-        position: item.location,
-        title: item.name,
-        icon: icon
-    });
+  const marker = new google.maps.Marker({
+    map,
+    position: item.location,
+    title: item.name,
+    icon,
+  });
 
-    currentMarkers.push(marker);
-    var contentString = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h1 id="firstHeading" class="firstHeading">' + item.name + '</h1>' +
-        '<div id="bodyContent">' +
-        '<img src="' + item.photos[0] + '"/>' +
-        '</div>' +
-        '</div>';
+  currentMarkers.push(marker);
+  const contentString = `
+  <div id="content">
+    <div id="siteNotice" />
+    <h1 id="firstHeading" class="firstHeading">${item.name}</h1>
+    <div id="bodyContent">
+      <img src="${item.photos[0]}" />
+    </div>
+  </div>
+  `;
 
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent(contentString);
-        infowindow.open(map, this);
-    });
-
-
+  google.maps.event.addListener(marker, 'click', () => {
+    infowindow.setContent(contentString);
+    infowindow.open(map, this);
+  });
 }
